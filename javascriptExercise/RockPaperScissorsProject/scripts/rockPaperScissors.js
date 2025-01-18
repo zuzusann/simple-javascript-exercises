d456beb3d962ef8adae2edec39c225cf334ad30d
zuzusann <zuzusan65@gmail.com>
2025-01-18 13:53:48 +0630

    [ADD]  Add keyboard shortcuts (r, p, s) for 'rock', 'paper', and 'scissors'

--- javascriptExercise/RockPaperScissorsProject/scripts/rockPaperScissors.js
+++ javascriptExercise/RockPaperScissorsProject/scripts/rockPaperScissors.js
@@ -22,6 +22,16 @@
       playGame('scissors');
     });
 
+    document.body.addEventListener('keydown', (event) => {
+      if(event.key === 'r'){
+        playGame('rock');
+      }else if(event.key === 'p'){
+        playGame('paper');
+      }else if(event.key === 's'){
+        playGame('scissors');
+      }
+    });
+
     function playGame(playerMove) {
       const computerMove = pickComputerMove();
 
