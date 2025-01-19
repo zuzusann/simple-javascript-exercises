ce9af2bb4c54bbb7cbb92bcacaa09f0142aa4d24
zuzusann <zuzusan65@gmail.com>
2025-01-19 15:00:23 +0630

    [ADD] use dom to make the cart quantity interactive

--- javascriptExercise/javascript-shopping-project/javascript-amazon-project-main/scripts/amazon.js
+++ javascriptExercise/javascript-shopping-project/javascript-amazon-project-main/scripts/amazon.js
@@ -81,9 +81,15 @@ document.querySelectorAll('.js-add-to-cart')
                 quantity: 1
                });
             }
-        });
-        console.log(cart);
+
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+              cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        });   
     });
 
-let matchingItem;
 
