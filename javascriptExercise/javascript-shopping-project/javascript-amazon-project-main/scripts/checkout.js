07c5ba805620a43f47c8acb20286aebcca2fd80a
zuzusann <zuzusan65@gmail.com>
2025-01-20 11:22:09 +0630

    [ADD] add cart to localStorage

--- javascriptExercise/javascript-shopping-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascriptExercise/javascript-shopping-project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,21 +1,21 @@
-import { cart, removeFromCart } from "../data/cart.js"; 
-import { products } from "../data/products.js";
-import { formatCurrency } from "./utils/money.js";
+import {cart, removeFromCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
-    const productId = cartItem.productId;
+  const productId = cartItem.productId;
 
-    let matchingProduct;
+  let matchingProduct;
 
-    products.forEach((product) => {
-        if(product.id === productId){
-            matchingProduct = product;
-        }
-    });
+  products.forEach((product) => {
+    if(product.id === productId) {
+      matchingProduct = product;
+    }
+  });
 
-    cartSummaryHTML += `
+  cartSummaryHTML += `
     <div class="cart-item-container
       js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
@@ -95,16 +95,18 @@ cart.forEach((cartItem) => {
   `;
 });
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+document.querySelector('.js-order-summary')
+  .innerHTML = cartSummaryHTML;
 
 document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-  
-      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
       container.remove();
     });
   });
-
